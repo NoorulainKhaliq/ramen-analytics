612060ccc6db75426ebf949c8d91a4a34147ab08
Noorulain Khaliq <noorulainakhaliq@gmail.com>
2018-01-29 16:06:21 -0500

    test added

--- server/test.spec.js
+++ server/test.spec.js
@@ -1,1 +1,36 @@
-var expect = require("chai").expect;
+const expect = require("chai").expect;
+const mocha = require("mocha");
+const request = require("supertest");
+const should = require("chai").should();
+const server = require("./api");
+
+let { parseData, maxMonthConsumption, getDate } = require("./functions");
+let testArray = require("./testArray");
+
+let newData = [
+  {
+    person: "bob",
+    "ramen-type": "kimchi",
+    date: "2015-08-02T02:00:00.000Z"
+  },
+  {
+    person: "bob",
+    "ramen-type": "kimchi",
+    date: "2015-08-02T02:00:00.000Z"
+  },
+  {
+    person: "bob",
+    "ramen-type": "kimchi",
+    date: "2015-08-02T02:00:00.000Z"
+  }
+];
+
+describe("checks day of max consumption in a month", () => {
+  let mockData = testArray.data;
+  let newArr = mockData.concat(newData);
+
+  it("should return most active day after adding new data", () => {
+    let maxConsumption = maxMonthConsumption(newArr);
+    expect(maxConsumption[2].mostActiveDay).to.equal("02");
+  });
+});
